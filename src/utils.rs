088ce26d5d24bbe73eb72d2f2e77e8a1240957ac
Rh4096 <rhelvetican@gmail.com>
2024-03-16 23:43:17 +0700

    wc

--- src/utils.rs
+++ src/utils.rs
@@ -1,13 +1,9 @@
-use image::{imageops::FilterType, open, DynamicImage, GenericImage, GenericImageView};
-use std::{
-    clone::Clone,
-    cmp::PartialEq,
-    ops::{Rem, Sub},
-};
+use image::{imageops::FilterType, open, DynamicImage, GenericImageView};
+use std::{clone::Clone, cmp::PartialEq, ops::Rem};
 
 fn gcd<T>(a: T, b: T) -> T
 where
-    T: Rem<Output = T> + Sub<Output = T> + PartialEq + From<u8> + Clone,
+    T: Rem<Output = T> + PartialEq + From<u8> + Clone,
 {
     if b == T::from(0) {
         a
@@ -32,24 +28,8 @@ pub fn pixelize(source: DynamicImage) -> DynamicImage {
     let ratio = gcd(width, height) / 8;
     let new_width = width / ratio;
     let new_height = height / ratio;
-    scale(
-        source.resize(new_width, new_height, FilterType::Nearest),
-        ratio,
-        width,
-        height,
-    )
-}
 
-fn scale(source: DynamicImage, rate: u32, w: u32, h: u32) -> DynamicImage {
-    let mut result = DynamicImage::new_rgb8(w, h);
-    for (x, y, col) in source.pixels() {
-        let x = x * rate;
-        let y = y * rate;
-        for i in 0..=rate {
-            for j in 0..=rate {
-                result.put_pixel(x + i, y + j, col);
-            }
-        }
-    }
-    result
+    source
+        .resize(new_width, new_height, FilterType::Nearest)
+        .resize(width, height, FilterType::Nearest)
 }
