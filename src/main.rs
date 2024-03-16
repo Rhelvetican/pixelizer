41ec1968573700eca18d44a6f9d6e6bc5e0e7e1e
Rh4096 <rhelvetican@gmail.com>
2024-03-16 21:50:15 +0700

    twix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
+mod utils;
+use std::{
+    fs::{read_dir, DirBuilder},
+    path::Path,
+};
+
 fn main() {
-    println!("Hello, world!");
+    if !Path::new("output").exists() {
+        DirBuilder::new().create("output").unwrap();
+    }
+    if !Path::new("input").exists() {
+        DirBuilder::new().create("input").unwrap();
+    }
+    let contents = read_dir("input").unwrap();
+
+    for file in contents {
+        let file = file.unwrap();
+        let path = file.path();
+        if utils::check_image(path.to_str().unwrap()) {
+            let img = utils::read_image(path.to_str().unwrap());
+            let output_path = format!("output/{}.png", path.file_stem().unwrap().to_str().unwrap());
+            let img = utils::pixelize(img);
+            img.save(&output_path).unwrap();
+            println!("Saved image to {}", output_path);
+        } else {
+            println!("Not an image. Skipping...")
+        }
+    }
 }
---
+++ src/utils.rs
@@ -0,0 +1,37 @@
+use image::{imageops::FilterType, open, DynamicImage, GenericImageView};
+use std::{
+    clone::Clone,
+    cmp::PartialEq,
+    ops::{Rem, Sub},
+};
+
+fn gcd<T>(a: T, b: T) -> T
+where
+    T: Rem<Output = T> + Sub<Output = T> + PartialEq + From<u8> + Clone,
+{
+    if b == T::from(0) {
+        a
+    } else {
+        gcd(b.clone(), a % b)
+    }
+}
+
+pub fn read_image(path: &str) -> DynamicImage {
+    match open(path) {
+        Ok(img) => img,
+        Err(e) => panic!("Error reading image: {}", e),
+    }
+}
+
+pub fn check_image(path: &str) -> bool {
+    open(path).is_ok()
+}
+
+pub fn pixelize(source: DynamicImage) -> DynamicImage {
+    let (width, height) = source.dimensions();
+    let ratio = gcd(width, height);
+    let new_width = width / ratio;
+    let new_height = height / ratio;
+    source.resize(new_width, new_height, FilterType::Nearest);
+    source.resize(width, height, FilterType::Gaussian)
+}
