1c51701138e7512493c9dfcae4897f0df99c98ab
Rh4096 <rhelvetican@gmail.com>
2024-03-18 16:47:23 +0700

    config

--- src/main.rs
+++ src/main.rs
@@ -12,6 +12,9 @@ fn main() {
     if !Path::new("input").exists() {
         DirBuilder::new().create("input").unwrap();
     }
+    if !Path::new("config").exists() {
+        DirBuilder::new().create("config").unwrap();
+    }
     let contents = read_dir("input").unwrap();
 
     for file in contents {
@@ -22,11 +25,15 @@ fn main() {
                     let img = utils::read_image(path.to_str().unwrap());
                     let output_path =
                         format!("output/{}.png", path.file_stem().unwrap().to_str().unwrap());
-                    let scale = match prompt_reply("Enter scale: ") {
-                        Ok(scale) => scale.parse::<u32>().unwrap(),
+                    let scale = match prompt_reply("Enter scale (Default to 8): ") {
+                        Ok(scale) => scale.parse::<u32>().unwrap_or(8),
                         Err(_) => 8,
                     };
-                    let img = utils::pixelize(img, scale);
+                    let radius = match prompt_reply("Enter radius (Default to 3): ") {
+                        Ok(radius) => radius.parse::<u32>().unwrap_or(3),
+                        Err(_) => 3,
+                    };
+                    let img = utils::pixelize(img, scale, radius);
                     img.save(&output_path).unwrap();
                     println!("Saved image to {}", output_path);
                 } else {
--- src/utils.rs
+++ src/utils.rs
@@ -1,5 +1,11 @@
-use image::{imageops::FilterType, open, DynamicImage, GenericImageView};
-use std::{clone::Clone, cmp::PartialEq, ops::Rem};
+use image::{imageops::FilterType, open, DynamicImage, GenericImageView, ImageBuffer};
+use imageproc::filter::median_filter;
+use serde::Serialize;
+use serde_json::{
+    from_reader, ser::PrettyFormatter, to_writer_pretty, Result as SerdeJsonResult, Serializer,
+    Value,
+};
+use std::{clone::Clone, cmp::PartialEq, fs::File, io::BufReader, ops::Rem, vec::Vec};
 
 fn gcd<T>(a: T, b: T) -> T
 where
@@ -23,21 +29,42 @@ pub fn check_image(path: &str) -> bool {
     open(path).is_ok()
 }
 
-pub fn pixelize(source: DynamicImage, scale: u32) -> DynamicImage {
+pub fn pixelize(source: DynamicImage, scale: u32, radius: u32) -> DynamicImage {
     let (width, height) = source.dimensions();
     let pixel_size = gcd(width, height);
     if pixel_size <= 1 {
         // Basically crop them image into either 16:9 or 9:20 aspect ratio
         let (rw, rh) = if width > height { (16, 9) } else { (9, 20) };
-        source
-            .resize_exact(rw * scale, rh * scale, FilterType::Nearest)
-            .resize(width, height, FilterType::Nearest)
+        let source: ImageBuffer<_, Vec<_>> = ImageBuffer::<image::Rgb<u8>, Vec<u8>>::from(
+            source.resize(rw * scale, rh * scale, FilterType::Nearest),
+        );
+        let source = median_filter(&source, radius, radius);
+        DynamicImage::from(source).resize(width, height, FilterType::Nearest)
     } else {
         let pixel_size = pixel_size / 8;
         let new_width = width / pixel_size;
         let new_height = height / pixel_size;
-        source
-            .resize(new_width, new_height, FilterType::Nearest)
-            .resize(width, height, FilterType::Nearest)
+        let source: ImageBuffer<_, Vec<_>> = ImageBuffer::<image::Rgb<u8>, Vec<u8>>::from(
+            source.resize(new_width, new_height, FilterType::Nearest),
+        );
+        let source = median_filter(&source, radius, radius);
+        DynamicImage::from(source).resize(width, height, FilterType::Nearest)
     }
+}
+
+pub fn read_json(path: &str) -> SerdeJsonResult<Value> {
+    let json_reader = BufReader::new(match File::open(path) {
+        Ok(file) => file,
+        Err(_) => panic!("Path {} not found.", path),
+    });
+    from_reader(json_reader)
+}
+
+pub fn write_json(path: &str, value: Value) -> SerdeJsonResult<()> {
+    let file = File::create(path).unwrap();
+    let fmt = PrettyFormatter::with_indent(b"    ");
+    let mut buf = Vec::new();
+    let mut ser = Serializer::with_formatter(&mut buf, fmt);
+    value.serialize(&mut ser).unwrap();
+    to_writer_pretty(file, &value)
 }
